295df10eb0c0985788f3a4c8c66dcfc4e8f0b3fe
mathias-yh-wong <yuihim.wong26@ycdsbk12.ca>
2025-05-30 09:15:52 -0400

    May 30th 2025 - Main menu setup
    -If statement for options (play, leaderboard, help, quit)
    -Used getChar to read keyboard
    -Added helpMenu method

--- CPTMathiasWong.java
+++ CPTMathiasWong.java
@@ -1,12 +1,36 @@
 import arc.*;
 
-public class setuptest{
+public class CPTMathiasWong{
 	public static void main(String[] args){
-		Console c = new Console();
+		
+		//Setting up user interface in console
+		Console c = new Console(/*int intWidth, int intHeight*/);
 		
 		//Main menu
-		playgame(c);
-		viewLeaderboard(c);
+		
+
+		
+		//Prompting user for input on what they want to do
+		c.println("Options - Enter:"); 
+		c.println("P for play");
+		c.println("L for leaderboard");
+		c.println("H for help");
+		c.println("Q for quit");
+		char charMain = c.getChar();
+		
+		//Directing to specific method
+		if(Character.toUpperCase(charMain) == 'P'){
+			playGame(c);
+		}else if(Character.toUpperCase(charMain) == 'L'){
+			viewLeaderboard(c);
+		}else if(Character.toUpperCase(charMain) == 'Q'){
+			c.closeConsole();
+		}else if(Character.toUpperCase(charMain) == 'H'){
+			helpMenu(c);
+		}else{
+			c.println("Not a valid input.");
+		}
+		
 	}
 	
 	public static void playGame(Console c){
@@ -14,6 +38,10 @@ public class setuptest{
 	}
 	
 	public static void viewLeaderboard(Console c){
+		
+	}
+	
+	public static void helpMenu(Console c){
 		
 	}
 	
