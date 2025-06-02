f3a8e19f491dbb05fa984abe5f984091c1ada74a
mathias-yh-wong <yuihim.wong26@ycdsbk12.ca>
2025-06-02 17:19:52 -0400

    2 June 2025 - Added consoleSetup method
    -Moved code for setting up graphics on Console to CPTMethods

--- CPTMethods.java
+++ CPTMethods.java
@@ -1,4 +1,7 @@
 import arc.*;
+import java.awt.Color;
+import java.awt.image.BufferedImage;
+import java.awt.Font;
 
 public class CPTMethods{
 	public static void main(String[] args){
@@ -6,10 +9,30 @@ public class CPTMethods{
 		
 	}
 	
-
-	
-
-	public static{
+	public static void consoleSetup(Console c){
+		
+		//Background colour
+		c.setBackgroundColor(Color.WHITE);
+		c.fillRect(0, 0, 1280, 720);
+		
+		//Logo
+		BufferedImage logo = c.loadImage("logo.jpg");
+		c.drawImage(logo, 50, 100);
+		
+		//Text + font setup
+		Font font = c.loadFont("font1.otf", 120);
+		c.setDrawFont(font);
+		c.setDrawColor(Color.BLACK);
+		c.drawString("BLACKJACK", 75, -10);
+		
+		//Main menu options + text/font setup
+		Font smallFont = c.loadFont("font1.otf", 60);
+		c.setDrawFont(smallFont);
+		c.drawString(" (P) Play", 700, 300);
+		c.drawString(" (L) Leaderboard", 700, 350);
+		c.drawString(" (H) Help", 700, 400);
+		c.drawString(" (Q) Quit", 700, 450);
+		c.repaint();
 		
 	}
 
