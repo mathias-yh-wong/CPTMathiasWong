7350757ab2afe9aa940f830a889e04a5bf1f7b86
mathias-yh-wong <yuihim.wong26@ycdsbk12.ca>
2025-06-03 12:03:39 -0400

    3 June 2025 - Finalizing deck of cards
    -Added code to CPTMethods for deckOfCards method
    -intDeck[][] 2D Array setup + values assigned + shuffled using a random integer
    -Used System.out.println to debug and list out the deck of cards in shuffled order

--- CPTMethods.java
+++ CPTMethods.java
@@ -39,7 +39,46 @@ public class CPTMethods{
 	public static int[][] deckOfCards(){
 		
 		int intDeck[][] = new int[52][3];
-		intDeck[0][0] = 0;
+		int intCards = 0;
+		int intValue = 0;
+		int intSuit = 0;
+		
+		for(intSuit = 0; intSuit <= 3; intSuit++){
+			for(intValue = 0; intValue <= 12; intValue++){
+				intDeck[intCards][0] = intValue;
+				intDeck[intCards][1] = intSuit;
+				intDeck[intCards][2] = (int) (Math.random() * 100 + 1);
+				intCards++;
+			}
+		}
+		
+		int intVTemp;
+		int intSTemp;
+		int intRTemp;
+		
+		for(int intCount2 = 0; intCount2 < intCards-1; intCount2++){
+			for(int intCount = 0; intCount < intCards-1; intCount++){
+				if(intDeck[intCount][2] > intDeck[intCount+1][2]){
+					// Swapping card value
+					intVTemp = intDeck[intCount][0];
+					intDeck[intCount][0] = intDeck[intCount+1][0];
+					intDeck[intCount+1][0] = intVTemp;
+					// Swapping suit value
+					intSTemp = intDeck[intCount][1];
+					intDeck[intCount][1] = intDeck[intCount+1][1];
+					intDeck[intCount+1][1] = intSTemp;
+					// Swapping random integer value
+					intRTemp = intDeck[intCount][2];
+					intDeck[intCount][2] = intDeck[intCount+1][2];
+					intDeck[intCount+1][2] = intRTemp;
+				}
+			}
+		}
+		
+		for(int intCount = 0; intCount < intCards; intCount++){
+			System.out.println(intDeck[intCount][0] + " - " + intDeck[intCount][1] + " - " + intDeck[intCount][2]);
+		}
+		
 		return intDeck;
 		
 	}
