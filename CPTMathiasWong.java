89344bdf8430af5c0be2a7b4944dcc80e5187d9b
mathias-yh-wong <yuihim.wong26@ycdsbk12.ca>
2025-06-03 09:38:41 -0400

    3 June 2025
    -Put all main menu code in a while loop to rerun code when returning to main menu
    -Added statitan cheat username in playGame method
    -Initialized intDeck[][] array, code is located in CPTMethods.deckOfCards() method
    -Added bubble sort to leaderboard array
    -Added option to return to main menu from leaderboard menu
    -Added secret menu option accessed by entering character 's'
    -Secret menu option located in funnyJoke method

--- CPTMathiasWong.java
+++ CPTMathiasWong.java
@@ -6,30 +6,32 @@ import java.awt.Font;
 public class CPTMathiasWong{
 	public static void main(String[] args){
 		
-		//Setting up console
-		Console c = new Console("Blackjack", 1280, 720);
-		CPTMethods.consoleSetup(c);
-		c.repaint();
+		boolean blnGame = true;
 		
-		//Getting character from user keyboard
-		char charMain = c.getChar();
-		System.out.println(charMain);
+		while(true){
 		
-		//Directing to specific method
-		if(Character.toUpperCase(charMain) == 'P'){
-			playGame(c);
+			//Setting up console
+			Console c = new Console("Blackjack", 1280, 720);
 			CPTMethods.consoleSetup(c);
-		}else if(Character.toUpperCase(charMain) == 'L'){
-			viewLeaderboard(c);
-			CPTMethods.consoleSetup(c);
-		}else if(Character.toUpperCase(charMain) == 'Q'){
-			c.closeConsole();
-		}else if(Character.toUpperCase(charMain) == 'H'){
-			helpMenu(c);
-			CPTMethods.consoleSetup(c);
+			c.repaint();
 			
-		}else{
-			c.println("Not a valid input.");
+			//Getting character from user keyboard
+			char charMain = c.getChar();
+			System.out.println("Entered character: "+charMain);
+			
+			//Directing to specific method
+			if(Character.toUpperCase(charMain) == 'P'){
+				playGame(c);
+			}else if(Character.toUpperCase(charMain) == 'L'){
+				viewLeaderboard(c);
+			}else if(Character.toUpperCase(charMain) == 'Q'){
+				c.closeConsole();
+			}else if(Character.toUpperCase(charMain) == 'H'){
+				helpMenu(c);
+			}else if(Character.toUpperCase(charMain) == 'S'){
+				funnyJoke(c);
+			}
+		
 		}
 		
 	}
@@ -46,6 +48,17 @@ public class CPTMathiasWong{
 		c.println("Enter your username");
 		String strName = c.readLine();
 		int intMoney = 1000;
+		
+		//"statitan" cheat
+		if(strName.equalsIgnoreCase("statitan")){
+			intMoney = 1000000;
+		}
+		
+		c.println("Your username is: "+strName);
+		c.println("Your money is: "+intMoney);
+		
+		//creating deck of cards array
+		int intDeck[][] = CPTMethods.deckOfCards();
 		
 		//Outputting username + money to leaderboard file
 		TextOutputFile leaderboard = new TextOutputFile("leaderboard.txt", true);
@@ -88,6 +101,38 @@ public class CPTMathiasWong{
 		
 		//sorting array
 		
+		String strPTemp;
+		String strSTemp;
+		
+		for(int intCount2 = 0; intCount2 < intPlayers-1; intCount2++){
+			for(int intCount = 0; intCount < intPlayers-1; intCount++){
+				// Convert people's score from string to integer and compare
+				if(Integer.parseInt(strLeaderboard[intCount][1]) < Integer.parseInt(strLeaderboard[intCount+1][1])){
+					// Swap occurs
+					// Swapping name
+					strPTemp = strLeaderboard[intCount][0];
+					strLeaderboard[intCount][0] = strLeaderboard[intCount+1][0];
+					strLeaderboard[intCount+1][0] = strPTemp;
+					// Swapping score
+					strSTemp = strLeaderboard[intCount][1];
+					strLeaderboard[intCount][1] = strLeaderboard[intCount+1][1];
+					strLeaderboard[intCount+1][1] = strSTemp;
+					
+				}
+			}
+		}
+		
+		for(int intCount = 0; intCount < intPlayers; intCount++){
+			c.println(strLeaderboard[intCount][0] + " - " + strLeaderboard[intCount][1]);
+		}
+		
+		c.println();
+		c.println("Enter spacebar to return to main menu.");
+		char charLead = c.getChar();
+		if(charLead == ' '){
+			c.clear();
+			return;
+		}
 		
 	}
 	
@@ -112,6 +157,7 @@ public class CPTMathiasWong{
 		
 		//prompt user to get character tp 
 		char charHelp = c.getChar();
+		System.out.println(charHelp);
 		c.clear();
 		
 		//displaying info for option selected + prompt return to help menu
@@ -186,11 +232,31 @@ public class CPTMathiasWong{
 				helpMenu(c);
 			}
 		}else if(charHelp == 'q' || charHelp == 'Q'){
-			//returns to main menu
 			c.clear();
 			return;
 		}
 		
+		
+	}
+	
+	public static void funnyJoke(Console c){
+		
+		//repainting console
+		c.setBackgroundColor(Color.WHITE);
+		c.fillRect(0, 0, 1280, 720);
+		c.repaint();
+		c.setTextColor(Color.BLACK);
+				
+		c.println("What do you call a magician who lost their magic?");
+		c.sleep(1000);
+		c.println("Ian!");
+		c.println();
+		c.println("Enter spacebar to return to the main menu");
+		char charJoke = c.getChar();
+		if(charJoke == ' '){
+			c.clear();
+			return;
+		}
 		
 	}
 	
