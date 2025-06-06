d5fe0fabf77cf30861ce813caebf5ca308b5d045
mathias-yh-wong <yuihim.wong26@ycdsbk12.ca>
2025-06-06 08:34:04 -0400

    5-6 June 2025
    -Added comments for code to clarify functionality
    -Reformatted spacing for conciseness
    -Declared + initialized multiple variables
    -Added player card count condition for instant blackjack win to prevent accidental trigger
    -Moved double down scenario into else statement
    -Added continue to prevent relooping
    -Added logic for hitting/staying
    -Added blnHit variable to stop loop if player decides to stay
    -Added 5 card win scenario
    -Added bust scenario
    -Added dealer's turn after player stays
    -Added logic comparing dealer and player's hand + scenarios that occur
    -Added scenario for if player selects to play again but doesn't have enough money
    -Replaced spacebar return with press any key

--- CPTMathiasWong.java
+++ CPTMathiasWong.java
@@ -9,7 +9,7 @@ public class CPTMathiasWong{
 		//boolean variable + loop to keep game running
 		boolean blnGame = true;
 		
-		while(true){
+		while(blnGame == true){
 		
 			//Setting up console
 			Console c = new Console("Blackjack", 1280, 720);
@@ -43,8 +43,11 @@ public class CPTMathiasWong{
 	public static void playGame(Console c){
 		
 		//declaring + initializing variables
+		//gameplay loop for play again
 		boolean blnPlay = true;
+		//username variable
 		String strName = "";
+		//money variable
 		int intMoney = 1000;
 		
 		//repainting console
@@ -63,7 +66,7 @@ public class CPTMathiasWong{
 		}
 		
 		//while loop to replay game 
-		while(blnPlay = true && intMoney > 0){
+		while(blnPlay == true && intMoney > 0){
 			
 			c.clear();
 			
@@ -71,12 +74,15 @@ public class CPTMathiasWong{
 			c.println("You have $" + intMoney + ". Place your bet:");
 			int intBet = c.readInt();
 			c.clear();
+			//if bet is bigger than they can afford
 			if(intMoney < intBet){
 				c.println("Not enough money.");
 				c.sleep(1000);
 				return;
 			}else if(intBet <= 0){
+				//if bet is invalid
 				c.println("Invalid bet.");
+				c.sleep(1000);
 				return;
 			}
 			intMoney = intMoney - intBet;
@@ -85,86 +91,202 @@ public class CPTMathiasWong{
 			int intDeck[][] = CPTMethods.deckOfCards();
 			
 			//dealing cards + declaring variables
+			
+			//player hand array
 			int[][] intPlayer = new int[5][2];
+			
+			//dealer hand array
 			int[][] intDealer = new int[5][2];
+			
+			//counts number of cards in player's hand
+			//2 at the start
 			int intPlayerCount = 2;
+			
+			//counts number of cards in dealer's hand
+			//2 at the start
 			int intDealerCount = 2;
+			
+			//order of the shuffled deck
+			//universal variable for both player and dealer
+			//0 is 1st card
 			int intDeckNum = 0;
-			boolean blnDoubleDown = true;
+			
+			//sum of player's hand
+			int intPlayerSum = 0;
+			
+			//sum of dealer's hand
+			int intDealerSum = 0;
+			
+			//player turn ends when double down
+			boolean blnDoubleDown = false;
+			
+			//variable to store text of player's current card
+			String strPlayerTemp;
+			
+			//variable to store text of dealer's current card
+			String strDealerTemp;
+			
 			
 			//loading data into intPlayer + intDealer arrays
 			for(int intCount = 0; intCount <= 1; intCount++){
+				//card 1 - player
 				intPlayer[intCount][0] = intDeck[intDeckNum][0];
+				//card 2 - player
 				intPlayer[intCount][1] = intDeck[intDeckNum++][1];
+				//card 1 - dealer
 				intDealer[intCount][0] = intDeck[intDeckNum][0];
+				//card 2 - dealer
 				intDealer[intCount][1] = intDeck[intDeckNum++][1];
 			}
 			
 			//dealer's first card is dealt face up on the table
 			c.println("The dealer's first card is: ");
-			String strDealerTemp = CPTMethods.cardText(intDealer[0][0], intDealer[0][1]);
+			//getting text for dealer's first card
+			strDealerTemp = CPTMethods.cardText(intDealer[0][0], intDealer[0][1]);
 			System.out.println("dealer card: "+strDealerTemp);
 			c.println(strDealerTemp);
 			c.println();
 			
 			//showing the player's hand
 			c.println("Your hand:");
+			//getting text for player's two cards
 			for(int intCount = 0; intCount <= 1; intCount++){
-				String strPlayerTemp = CPTMethods.cardText(intPlayer[intCount][0], intPlayer[intCount][1]);
+				strPlayerTemp = CPTMethods.cardText(intPlayer[intCount][0], intPlayer[intCount][1]);
 				c.println(strPlayerTemp);
 				System.out.println("player card: "+strPlayerTemp);
 			}
 			c.println();
 			
 			//calculating sum of player's hand in CPTMethods
-			int intPlayerSum = CPTMethods.handValue(intPlayer, intPlayerCount);
+			intPlayerSum = CPTMethods.handValue(intPlayer, intPlayerCount);
 			System.out.println("sum: "+intPlayerSum);
 			
 			//blackjack scenario
-			if(intPlayerSum == 21){
+			if(intPlayerSum == 21 && intPlayerCount == 2){
 				c.println("BLACKJACK! You get 3x your bet.");
 				intMoney = intMoney + intBet * 3;
 				System.out.println("money: "+intMoney);
+				continue;
 			}else{
-				blnDoubleDown = false;
-			}
-			
-			//double down scenario
-			if(intPlayerSum == 9 || intPlayerSum == 10 || intPlayerSum == 11){
+				if(intPlayerSum == 9 || intPlayerSum == 10 || intPlayerSum == 11){
 				c.println("Your total is " + intPlayerSum + ". Do you want to double down? (Y/N)");
 				char charDoubleDown = c.getChar();
 				if(Character.toUpperCase(charDoubleDown) == 'Y'){
 					intBet = intBet * 2;
 					
+					//load data for player's third card
 					intPlayer[intPlayerCount][0] = intDeck[intDeckNum][0];
-					intPlayer[intPlayerCount][0] = intDeck[intDeckNum][1];
+					intPlayer[intPlayerCount][1] = intDeck[intDeckNum][1];
 					
-					String strDDCard = CPTMethods.cardText(intPlayer[intPlayerCount-1][0], intPlayer[intPlayerCount][1]);
+					//get text for player's third card
+					String strDDCard = CPTMethods.cardText(intPlayer[intPlayerCount][0], intPlayer[intPlayerCount][1]);
+					//incrementing deckNum so doesn't deal the same card
 					intDeckNum++;
+					//incrementing playerCount to represent number of cards player hands
 					intPlayerCount++;
-					
+
 					c.println("Your new card is: "+strDDCard);
+					//calculating new value of player's hand
 					intPlayerSum = CPTMethods.handValue(intPlayer, intPlayerCount);
+					System.out.println("new sum: "+intPlayerSum);
 					c.println("Your hand's total is: "+intPlayerSum);
+					//setting double down to true and ending player's turn
 					blnDoubleDown = true;
+					continue;
+					}
 				}
 			}
 			
 			//hit or stay
 			c.println();
-			c.println("hit or stay pick one");
-			char charHitStay = c.getChar();
-			if(Character.toUpperCase(charHitStay) == 'h'){
-				c.closeConsole();
-			}else if(Character.toUpperCase(charHitStay) == 's'){
-				c.closeConsole();
+			if(blnDoubleDown == false && intPlayerSum < 21){
+				boolean blnHit = true;
+				while(blnHit == true && intPlayerSum < 21 && intPlayerCount < 5){
+					c.println("Hit or Stay? 1/2");
+					char charHitStay = c.getChar();
+					if(charHitStay == '1'){
+						//load data for player's third card
+						intPlayer[intPlayerCount][0] = intDeck[intDeckNum][0];
+						intPlayer[intPlayerCount][1] = intDeck[intDeckNum][1];
+						
+						strPlayerTemp = CPTMethods.cardText(intPlayer[intPlayerCount][0], intPlayer[intPlayerCount][1]);
+						c.println("Your new card is: "+strPlayerTemp);
+						
+						//incrementing deckNum so it pulls next card
+						intDeckNum++;
+						//incrementing playerCount so it updates number of cards in player's hand
+						intPlayerCount++;
+						
+						//calculating new value of player's hand
+						intPlayerSum = CPTMethods.handValue(intPlayer, intPlayerCount);
+						
+						System.out.println("new sum: "+intPlayerSum);
+						c.println("Your hand's total is: "+intPlayerSum);
+					}else{
+						blnHit = false;
+					}
+				}
+			}
+			
+			if(intPlayerCount == 5 && intPlayerSum <= 21){
+				c.println("You got 5 cards! You automatically win 3x your bet.");
+				intMoney = intMoney + intBet * 3;
+				c.println("Press any key to continue.");
+				c.getChar();
+				System.out.println("money: "+intMoney);
+				continue;
+			}else if(intPlayerSum > 21){
+				c.println("You busted! Your sum is over 21.");
+			}else{
+				//calculating sum of dealer's hand
+				intDealerSum = CPTMethods.handValue(intDealer, intDealerCount);
+				//loop checking and allowing dealer to draw until sum is over 17
+				while(intDealerSum < 17){
+					//load data for dealer's third card
+					intDealer[intDealerCount][0] = intDeck[intDeckNum][0];
+					intDealer[intDealerCount][1] = intDeck[intDeckNum][1];
+					intDeckNum++;
+					intDealerCount++;
+					intDealerSum = CPTMethods.handValue(intDealer, intDealerCount);
+				}
+				
+				//revealing dealer's hand
+				c.println();
+				c.println("Dealer's hand is: ");
+				for(int intCount = 0; intCount < intDealerCount; intCount++){
+					strDealerTemp = CPTMethods.cardText(intDealer[intCount][0], intDealer[intCount][1]);
+					c.println(strDealerTemp);
+				}
+				c.println();
+				c.println("The dealer's sum is: "+intDealerSum);
+				c.sleep(1000);
+				
+				//comparing player's sum and dealer's sum
+				if(intDealerSum > 21 || intPlayerSum > intDealerSum){
+					c.println("You win!");
+					intMoney = intMoney + intBet * 2;
+				}else if(intPlayerSum == intDealerSum){
+					c.println("A tie. Your bet is returned.");
+					intMoney = intMoney + intBet;
+				}else{
+					c.println("You lose.");
+				}
+				System.out.println("money: "+intMoney);
+					
 			}
 			
 			//asking if user wants to play again
+			c.println();
 			c.println("Play Again? Y/N");
 			char charPlayAgain = c.getChar();
 			if(Character.toUpperCase(charPlayAgain) == 'Y'){
 				blnPlay = true;
+				if(intMoney <= 0){
+					c.println("Not enough money left to play again.");
+					c.println("Press any key to return to menu.");
+					c.getChar();
+					return;
+				}
 			}else if(Character.toUpperCase(charPlayAgain) == 'N'){
 				blnPlay = false;
 			}
@@ -240,12 +362,9 @@ public class CPTMathiasWong{
 		
 		//prompting user to return to main menu
 		c.println();
-		c.println("Enter spacebar to return to main menu.");
-		char charLead = c.getChar();
-		if(charLead == ' '){
-			c.clear();
-			return;
-		}
+		c.println("Press any key to return to main menu.");
+		c.getChar();
+		return;
 		
 	}
 	
